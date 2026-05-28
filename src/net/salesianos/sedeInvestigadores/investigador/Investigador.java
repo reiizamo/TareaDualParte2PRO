36db6c03b2ee59a3e002eeb4531f34f5dc04f8b1
Asier Zamora <asierferzam@gmail.com>
2026-05-28 10:16:01 +0100

    atributos y constructores de la clase investigador

--- src/net/salesianos/sedeInvestigadores/investigador/Investigador.java
+++ src/net/salesianos/sedeInvestigadores/investigador/Investigador.java
@@ -1,5 +1,17 @@
 package net.salesianos.sedeInvestigadores.investigador;
 
 public class Investigador {
+    private String nombre;
+    private String especialidad;
+    private int sueldo;
+
+    public Investigador(){}
+
+    public Investigador(String nombre, String especialidad, int sueldo){
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.sueldo = sueldo;
+    }
+
     
 }
