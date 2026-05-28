17ff9eb5f88e102f0fce989bb2e3024b7aed76c2
Asier Zamora <asierferzam@gmail.com>
2026-05-28 10:26:49 +0100

    getters y setters de la clase investigador

--- src/net/salesianos/sedeInvestigadores/investigador/Investigador.java
+++ src/net/salesianos/sedeInvestigadores/investigador/Investigador.java
@@ -13,5 +13,23 @@ public abstract class Investigador {
         this.sueldo = sueldo;
     }
 
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getEspecialidad(){
+        return this.especialidad;
+    }
+    public int getSueldo(){
+        return this.sueldo;
+    }
 
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setEspecialidad(String especialidad){
+        this.especialidad = especialidad;
+    }
+    public void setSueldo(int sueldo){
+        this.sueldo = sueldo;
+    }
 }
