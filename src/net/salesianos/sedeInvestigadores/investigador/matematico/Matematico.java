209ca9fcbbb5413222b8b97e7c259bdcac7de3fb
Asier Zamora <asierferzam@gmail.com>
2026-05-28 12:36:19 +0100

    atributos y construtor de la clase matematico

--- src/net/salesianos/sedeInvestigadores/investigador/matematico/Matematico.java
+++ src/net/salesianos/sedeInvestigadores/investigador/matematico/Matematico.java
@@ -1,5 +1,15 @@
 package net.salesianos.sedeInvestigadores.investigador.matematico;
+import net.salesianos.sedeInvestigadores.investigador.Investigador;
 
-public class Matematico {
+public class Matematico extends Investigador{
+    
+    private int[][] resultadosActuales;
+
+    public Matematico(){}
+
+    public Matematico(String nombre, String especialidad, int sueldo){
+        super(nombre, especialidad, sueldo);
+    }
+
     
 }
