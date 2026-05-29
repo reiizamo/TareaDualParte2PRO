69c5be5aca98a4d874257fb46838be5e823c89a4
Asier Zamora <asierferzam@gmail.com>
2026-05-29 16:42:30 +0100

    menuMatematico

--- src/App.java
+++ src/App.java
@@ -1,5 +1,41 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import net.salesianos.sedeInvestigadores.investigador.biologo.Biologo;
+import net.salesianos.sedeInvestigadores.investigador.matematico.Matematico;
+import net.salesianos.sedeInvestigadores.investigador.quimico.Quimico;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Matematico matematico = new Matematico("Finn", "Algebra", 2000);
+        Biologo biologo = new Biologo("Oriday", "Marina", 1600);
+        Quimico quimico = new Quimico("Walter", "Organica", 1300);
+
     }
+
+    private static void menuMatematico(Matematico mate, Scanner sc) {
+        try {
+            System.out.print("\n--- MENÚ MATEMÁTICO ---\n");
+            System.out.print("    1. Estado Sueldo\n");
+            System.out.print("    2. Trabajar\n");
+
+            int op = sc.nextInt();
+
+            switch (op) {
+                case 1 :
+                    System.out.println(mate.estadoSueldo());
+                    break;
+                case 2 :
+                    System.out.println(mate.trabajar());
+                    break;
+                default :
+                    System.out.println("Opción no válida");
+                    break;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Error: entrada incorrecta");
+        }
+
+    }
+
 }
