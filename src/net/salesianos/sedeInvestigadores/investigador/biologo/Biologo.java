8316d3ac23d3c1053ac909c51571252720386de9
Asier Zamora <asierferzam@gmail.com>
2026-05-29 15:28:56 +0100

    atributos y constructores

--- src/net/salesianos/sedeInvestigadores/investigador/biologo/Biologo.java
+++ src/net/salesianos/sedeInvestigadores/investigador/biologo/Biologo.java
@@ -1,5 +1,15 @@
 package net.salesianos.sedeInvestigadores.investigador.biologo;
+import java.util.ArrayList;
+import net.salesianos.sedeInvestigadores.investigador.Investigador;
 
-public class Biologo {
+public class Biologo extends Investigador{
     
+    private ArrayList<String> especimenes = new ArrayList<>();
+
+    public Biologo(){}
+
+    public Biologo(String nombre, String especialidad, int sueldo){
+        super(nombre, especialidad, sueldo);
+    }
+
 }
